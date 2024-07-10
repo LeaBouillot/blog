08de2fc630c2c61867fb72ce9438e09988714b54
bestdevelope <leabouillot7@gmail.com>
2024-07-10 20:59:55 +0200

    routes

--- start/routes.ts
+++ start/routes.ts
@@ -7,20 +7,24 @@
 |
 */
 
+import LoginController from '#controllers/auth/login_controller'
+import LogoutController from '#controllers/auth/logout_controller'
+import RegisterController from '#controllers/auth/register_controller'
 import router from '@adonisjs/core/services/router'
 
-router.on('/').render('pages/home')
+router.on('/').render('pages/home').as('home')
 
-router.group(() =>  {
+router
+  .group(() => {
+    // Registration
+    router.get('/register', [RegisterController, 'show']).as('register.show')
+    router.post('/register', [RegisterController, 'store']).as('register.store')
 
-//Registration
-router.get('/register', [RegisterController, 'show']).as('resister.show')
-router.post('/register', [RegisterController, 'store']).as('resister.store')
+    // Login
+    router.get('/login', [LoginController, 'show']).as('login.show')
+    router.post('/login', [LoginController, 'store']).as('login.store')
 
-//Login
-router.get('/login', [LoginController, 'show']).as('login.show')
-router.post('/login', [LoginController, 'store']).as('login.store')
-
-//Logout
-router.get('/logout', [LogoutController, 'getOut']).as('logout')
-}).as('auth')
+    // Logout
+    router.post('/logout', [LogoutController, 'getOut']).as('logout')
+  })
+  .as('auth')
